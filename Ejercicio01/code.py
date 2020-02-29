92bfa84259b768e2b8100089575a9a2ac67daccf
RBorja21 <borja21rb@gmail.com>
2020-02-28 22:59:14 -0600

    Cambios menores

--- Ejercicio01/code.py
+++ Ejercicio01/code.py
@@ -4,6 +4,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime       #To access datetime
+from pandas import Series           #To work on series
+import warnings                     #To ignote the warnings
+warnings.filterwarnings("ignore")
 
-dataTrain = "/ruta/dataTrain"
+
+train = pd.read_csv("Train_SU63ISt.csv")
+test  = pd.read_csv("Test_0qrQsBZ.csv")
+
+# Realizo una copia de los datos de entrenamiento y prueba para que,
+# si realizo cambios en este conjunto de datos, no pierda
+# el conjunto de datos original
+
+train_original = train.copy()
+test_original  = test.copy()
+
 
